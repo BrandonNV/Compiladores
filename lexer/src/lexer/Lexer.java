d3573e567e589cad89943cea9f6651c419110494
Rafael Lozano <rafael.lozano.espinosa@gmail.com>
2018-09-06 14:07:39 -0500

    lexer original

--- lexer/src/lexer/Lexer.java
+++ lexer/src/lexer/Lexer.java
@@ -19,47 +19,44 @@ public class Lexer {
 		reserve(new Word(Tag.FALSE, "false"));
 	}
 
-	
 	public Token scan() throws IOException {
-		while (true) {
-			/* salta espacios en blanco */
-			for (;peek == ' '; peek = (char) System.in.read()) {
-				if (peek == ' ' || peek == '\t')
-					continue;
-				else if (peek == '\n')
-					line = line + 1;
-				else
-					break;
-			}
-			
-			/* Para generar un entero */
-			if (Character.isDigit(peek)) {
-				int v = 0;
-				do {
-					v = 10 * v + Character.digit(peek, 10);
-					peek = (char) System.in.read();
-				} while (Character.isDigit(peek));
-				return new Num(v);
-			}
-
-			/* Para generar un identificador */
-			if (Character.isLetter(peek)) {
-				StringBuffer b = new StringBuffer();
-				do {
-					b.append(peek);
-					peek = (char) System.in.read();
-				} while (Character.isLetterOrDigit(peek));
-				String s = b.toString();
-				Word w = (Word) words.get(s);
-				if (w != null)
-					return w;
-				w = new Word(Tag.ID, s);
-				words.put(s, w);
-				return w;
-			}
-			Token t = new Token(peek);
-			peek = ' ';
-			return t;
+		/* salta espacios en blanco */
+		for (; peek == ' '; peek = (char) System.in.read()) {
+			if (peek == ' ' || peek == '\t')
+				continue;
+			else if (peek == '\n')
+				line = line + 1;
+			else
+				break;
 		}
+
+		/* Para generar un entero */
+		if (Character.isDigit(peek)) {
+			int v = 0;
+			do {
+				v = 10 * v + Character.digit(peek, 10);
+				peek = (char) System.in.read();
+			} while (Character.isDigit(peek));
+			return new Num(v);
+		}
+
+		/* Para generar un identificador */
+		if (Character.isLetter(peek)) {
+			StringBuffer b = new StringBuffer();
+			do {
+				b.append(peek);
+				peek = (char) System.in.read();
+			} while (Character.isLetterOrDigit(peek));
+			String s = b.toString();
+			Word w = (Word) words.get(s);
+			if (w != null)
+				return w;
+			w = new Word(Tag.ID, s);
+			words.put(s, w);
+			return w;
+		}
+		Token t = new Token(peek);
+		peek = ' ';
+		return t;
 	}
 }
